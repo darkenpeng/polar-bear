3a98ee555f3c01afdc2105725f19f8f65b0b0810
chloeeee210 <chloeseo0210@gmail.com>
2022-03-08 14:34:51 +0900

    특정채널에서만 interaction하게 제한하기

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const { token, channelID } = require('./config.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const { channel } = require('diagnostics_channel');
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -13,31 +12,10 @@ for (const file of commandFiles) {
 	
 }
 
-// 먼저 fetch로 채널의 id를 이용해 요청한다
-// 
-// client.on('debug', console.error);
-//when new promise is created, the executor runs automatically.
-
-// const cachedChannel = client.channels.cache.get(channel)
-
-	// const cachedChannel = client.channels.cache.get(channel)
-	
-  	// .then(channel => console.log(channel))
-  	// .catch(console.error);
-
-// // 굳이 분리하지 않기
-// channel.send('content');
-
 client.on('ready', () => {
-
-	console.log('Ready!');
 	const cachedChannel = client.channels.cache.get(channelID);
-	// (property) DataManager<string, AnyChannel, ChannelResolvable>.cache: Collection<string, AnyChannel>
-    console.log(cachedChannel);
-	// client.channels.fetch(channelID)
-	// .then(console.log)
-	// .catch(console.error)
-
+	cachedChannel.send('메세지가 특정한 채널에 보내지는지 확인하는 메세지')
+	console.log('Ready!');
 	
 });
 
@@ -47,24 +25,31 @@ client.on('ready', () => {
 // url 갖고와서 og로 넘겨주는 친구 
 
 client.on('interactionCreate', async interaction => {
+	const cachedChannel = client.channels.cache.get(channelID);
+	console.log(cachedChannel.id)
+	console.log(interaction.channelId)
+	if(cachedChannel.id == interaction.channelId){
+		if (!interaction.isCommand()) return;
 	
-	if (!interaction.isCommand()) return;
-
-
-	// const channel = client.channels.cache.get('id');
-	// channel.send();
-	const command = client.commands.get(interaction.commandName);
 	
-	if (!command) return;
+		// const channel = client.channels.cache.get('id');
+		// channel.send();
+		const command = client.commands.get(interaction.commandName);
+		
+		if (!command) return;
+	
+		try {
+	
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
 client.login(token);
+
 
 
